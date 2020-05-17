c9294f688bee614544e3ceb42578869864da2b68
JXKENG <jxkeng119@gmail.com>
2020-05-18 01:42:21 +0800

    add build file

---
+++ build/precache-manifest.2351447e7e3019b94d031fea9d3cf6d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656f7e6f2502b2063fa782a374ee317b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa4ff77c5c05af42add",
+    "url": "/static/js/2.e0a31747.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.e0a31747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b694274ad80a0a6790",
+    "url": "/static/js/main.101d2cfa.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
